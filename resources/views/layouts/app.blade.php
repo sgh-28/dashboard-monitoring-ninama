f402f397e9f7eb3716d24ddd509866a54911a8f5
Singgit <Singgit@gmail.com>
2026-07-18 14:11:50 +0700

    Use Ninama logo as favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-ninama.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo-ninama.png') }}">
     <title>@yield('title', 'Business Dashboard - Ninama')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script>
